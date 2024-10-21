24d4ce6dc9e26765b7f7038898422be43290e994
Surajphirke3 <Surajphirke3@gmail.com>
2024-10-21 07:53:55 +0530

    after add all req page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,29 @@
-
-
- 
-
+import MobileNav from "@/components/ui/MobileNav";
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Suraj', lastName: 'phirke' };
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+       <Sidebar user={loggedIn}/>
+       <div className="flex size-full flex-col">
+        <div className="root-layout">
+        <Image
+        src="/icons/logo.svg"
+        width={30}
+        height={30}
+        alt="Logo" />
+        <div>
+          <MobileNav user={loggedIn}/>
+        </div>
+        </div>
+         {children}
+       
+       </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
 
+import HeaderBox from '@/components/ui/HeaderBox'
+import RightSidebar from '@/components/ui/RightSidebar'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn ={firstName :'Suraj',lastName:'phirke',email:'surajphrike3@gmail.com'}
   return (
-    <div>Home new</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header>
+                <HeaderBox
+                 type="greeting"
+                 title="Welcome "
+                 user={loggedIn?.firstName||'guest'}
+                 subtext="Access and manage user account and transactions efficiently"
+                />
+                <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={5500.20}
+                />
+                </header>
+
+                recent transcation 
+
+        </div>
+        <RightSidebar
+            user={loggedIn}
+            transactions={[]}
+            banks={[]}       
+        />
+    </section>
   )
 }
 
